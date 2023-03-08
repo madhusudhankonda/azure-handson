7c16900a4be40fec4e18dc77821ffdbdac1f31a4
Madhusudhan Konda <KondaM@ebrd.com>
2023-03-08 11:07:53 +0000

    added additional endpoint

--- breaking-news/src/main/java/com/chocolateminds/azurehandson/breakingnews/BreakingNewsController.java
+++ breaking-news/src/main/java/com/chocolateminds/azurehandson/breakingnews/BreakingNewsController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BreakingNewsController {
 
+    @GetMapping("/")
+    public String getDefaultNews(){
+        return "<h1>Default News </h1>";
+    }
+
     @GetMapping("/breaking")
     public String getBreakingNews(){
         return "<h1>Breaking News </h1>";
