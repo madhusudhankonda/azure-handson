8373726faeab1a8d4542e0e2c0b7f853b012faa3
Madhusudhan Konda <KondaM@ebrd.com>
2023-03-08 10:35:32 +0000

    Breaking news module

---
+++ breaking-news/src/main/java/com/chocolateminds/azurehandson/breakingnews/BreakingNewsApplication.java
@@ -0,0 +1,13 @@
+package com.chocolateminds.azurehandson.breakingnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakingNewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakingNewsApplication.class, args);
+    }
+
+}
